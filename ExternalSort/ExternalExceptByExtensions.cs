eb2c916cc272c9fa7da5bd5606c0f0601f7f7938
Clay Lenhart <clay@lenharts.net>
2024-04-08 16:06:53 -0400

    comments & 1.0.0-beta005

--- ExternalSort/ExternalExceptByExtensions.cs
+++ ExternalSort/ExternalExceptByExtensions.cs
@@ -5,9 +5,9 @@ namespace ExternalSort;
 public static class ExternalExceptByExtensions
 {
     /// <summary>
-    /// Performs a ExceptBy on data that is too large for RAM.  This works like a 'NOT IN' SQL clause.
+    /// Performs a ExceptBy on data that is too large for RAM.  This works like a 'DISTINCT' + 'NOT IN' SQL clause.
     /// Internally, it uses OrderByExternal() to sort both inputs and perform the except-by.
-    /// Duplicate keys in the first input are deduplicated, in which case a random row is selected. (I believe this matches ExceptBy())
+    /// Duplicate keys in the first input are deduplicated, in which case a random row is selected. (in order to match ExceptBy() behaviour)
     /// </summary>
     public static IExternalExceptByAsyncEnumerable<TSource> ExceptByExternal<TSource, TKey>(
         this IAsyncEnumerable<TSource> first,
